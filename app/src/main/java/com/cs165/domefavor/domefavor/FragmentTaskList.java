37d89bb0f4163d165c4f47a538e29e59570a0761
Jilai Zhou <jilai.chou@gmail.com>
2016-05-23 16:59:50 -0400

    add FAB and swiperefreshlayout

--- app/src/main/java/com/cs165/domefavor/domefavor/FragmentTaskList.java
+++ app/src/main/java/com/cs165/domefavor/domefavor/FragmentTaskList.java
@@ -2,59 +2,141 @@ package com.cs165.domefavor.domefavor;
 
 import android.app.Fragment;
 import android.app.ListFragment;
+import android.app.LoaderManager;
 import android.content.Context;
+import android.content.Loader;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.support.design.widget.FloatingActionButton;
 
 import java.util.ArrayList;
+
+//import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 /**
  *
  * Created by Jilai Zhou on 5/19/2016.
  */
-public class FragmentTaskList extends ListFragment {
+public class FragmentTaskList extends ListFragment implements SwipeRefreshLayout.OnRefreshListener,
+        LoaderManager.LoaderCallbacks<ArrayList<TaskItem>>, View.OnClickListener{
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private ListView mListView;
     private TaskListAdapter mTaskListAdapter;
     private View view;
+    private FloatingActionButton mFAB;
     private ArrayList<TaskItem> mTaskItemList;
 
 
     public void onCreate(Bundle mBundle){
         super.onCreate(mBundle);
-        mTaskListAdapter = new TaskListAdapter(getActivity());
-        setListAdapter(mTaskListAdapter);
-        //for testing
-        mTaskItemList = Utilities.createListForTest();
     }
 
 
     //inflate the fragment in the UI
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         view = inflater.inflate(R.layout.fragment_tasklist, container, false);
-        Button refreshBtn = (Button) view.findViewById(R.id.refreshButton);
-        refreshBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Location loc = Utilities.getLocation(getActivity());
-                Log.d("TaskListFrag", "lat: " + loc.getLatitude() + "; long: " + loc.getLongitude());
-                try {
-                    Server.getAllTasks(String.valueOf(loc.getLatitude()), String.valueOf(loc.getLongitude()));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+
+        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeTaskList);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+
+        mListView = (ListView) view.findViewById(android.R.id.list);
+        mTaskListAdapter = new TaskListAdapter(getActivity());
+        mListView.setAdapter(mTaskListAdapter);
+
+        mFAB = (FloatingActionButton)view.findViewById(R.id.fab);
+        setupFAB();
+        getLoaderManager().initLoader(0, null, this);
         return view;
+    }
+
+    @Override
+    public void onRefresh() {
+        Log.d("FragTaskList", "I'm on refresh");
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<ArrayList<TaskItem>> onCreateLoader(int id, Bundle args) {
+        return new TaskListLoader(getActivity());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<ArrayList<TaskItem>> loader, ArrayList<TaskItem> data) {
+        if (mSwipeRefreshLayout.isRefreshing()){
+            mSwipeRefreshLayout.setRefreshing(false);
+        }
+        mTaskListAdapter.clear();
+        mTaskListAdapter.addAll(data);
+        mTaskListAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<ArrayList<TaskItem>> loader) {
+        mTaskListAdapter.clear();
+    }
+
+    private void setupFAB(){
+        ImageView iconSortName = new ImageView(getActivity());
+        iconSortName.setImageResource(R.drawable.ic_action_alphabets);
+
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(getActivity());
+        itemBuilder.setBackgroundDrawable(getResources().getDrawable(R.drawable.selector_sub_button_gray));
+
+        SubActionButton buttonSortName = itemBuilder.setContentView(iconSortName).build();
+        buttonSortName.setTag("TEST");
+        buttonSortName.setOnClickListener(this);
+
+        FloatingActionMenu mFABMenu = new FloatingActionMenu.Builder(getActivity())
+                .addSubActionView(buttonSortName)
+                .attachTo(mFAB)
+                .build();
+
+
+    }
+
+    /**
+     * onClick method for SubActionButton in Floating Action Button
+     */
+    @Override
+    public void onClick(View v) {
+
     }
 
     //ListAdapter may use Weiqiang's class.
     public class TaskListAdapter extends ArrayAdapter<TaskItem> {
         public TaskListAdapter(Context context){
             super(context,android.R.layout.two_line_list_item);
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = (LayoutInflater)getContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View listItemView = convertView;
+            if (convertView==null)
+                listItemView = inflater.inflate(R.layout.tasklist_single_item, null);
+
+            TextView lineOneView = (TextView)listItemView.findViewById(R.id.textview1);
+            TextView lineTwoView = (TextView)listItemView.findViewById(R.id.textview2);
+
+            TaskItem task = getItem(position);
+
+            lineOneView.setText(task.getTaskName());
+            lineTwoView.setText(task.getTime());
+
+            return listItemView;
         }
 
     }
---
+++ app/src/main/java/com/cs165/domefavor/domefavor/TaskListLoader.java
@@ -0,0 +1,26 @@
+package com.cs165.domefavor.domefavor;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * Created by Jilai Zhou on 5/23/2016.
+ */
+public class TaskListLoader extends AsyncTaskLoader <ArrayList<TaskItem>> {
+    public TaskListLoader(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad(); //Force an asynchronous load.
+    }
+
+    @Override
+    public ArrayList<TaskItem> loadInBackground() {
+        return Utilities.createListForTest();
+    }
+}
