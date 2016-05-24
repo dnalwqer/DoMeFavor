0f3285214589fdb4410256ddcee89ebcb4c11401
brocolli <gaixuetong@hotmail.com>
2016-05-23 20:10:16 -0400

    server

---
+++ backend/src/main/java/com/cs165/domefavor/domefavor/backend/Mail.java
@@ -0,0 +1,70 @@
+package com.cs165.domefavor.domefavor.backend;
+
+/**
+ * Created by Alex on 16/5/20.
+ */
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * 发送邮件的测试程序
+ *
+ * @author lwq
+ *
+ */
+public class Mail {
+
+    public static void sendEmail(String receiver, String subject, String content){
+        try {
+            String to = receiver;
+
+            String msg ="email text....";
+            final String from ="gaixuetong@163.com";
+            final  String password ="";
+
+
+            Properties props = new Properties();
+            props.setProperty("mail.transport.protocol", "smtp");
+            props.setProperty("mail.host", "smtp.163.com");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.port", "25");
+//            props.put("mail.debug", "true");
+//            props.put("mail.smtp.socketFactory.port", "465");
+//            props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+//            props.put("mail.smtp.socketFactory.fallback", "false");
+            Session session = Session.getDefaultInstance(props,
+                    new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(from,password);
+                        }
+                    });
+
+            //session.setDebug(true);
+            Transport transport = session.getTransport();
+            InternetAddress addressFrom = new InternetAddress(from);
+
+            MimeMessage message = new MimeMessage(session);
+            message.setSender(addressFrom);
+            message.setSubject(subject);
+            message.setContent(msg, "text/plain");
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            transport.connect();
+            Transport.send(message);
+            transport.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
